57d7bb4d056ba891d053a9c3369ac9a66780a660
Ben Heidemann <ben@heidemann.co.uk>
2024-06-19 16:32:58 -0400

    chore: format files

--- src/utils/supportsColor.ts
+++ src/utils/supportsColor.ts
@@ -17,7 +17,7 @@ export function supportsColorDeno(): boolean {
 
 export function supportsColorNode(): boolean {
   return typeof global.process.stdin?.fd === "number" &&
-  global.tty.isatty(global.process.stdin.fd) && !global.process.env.NO_COLOR;
+    global.tty.isatty(global.process.stdin.fd) && !global.process.env.NO_COLOR;
 }
 export function supportsColorUnknownRuntime(): boolean {
   return false;
