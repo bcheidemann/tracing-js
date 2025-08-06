8f5f3a727b2f8b743f9c848194446e77c68065f8
Ben Heidemann <ben@heidemann.dev>
2025-08-06 18:41:47 +0100

    test: add missing tests for OpenTelemetrySubscriber

--- src/__tests__/subscribers/OpenTelemetrySubscriber.test.ts
+++ src/__tests__/subscribers/OpenTelemetrySubscriber.test.ts
@@ -3,6 +3,8 @@ import { expect } from "expect";
 import { event } from "../../event.ts";
 import { span } from "../../span.ts";
 import { Level } from "../../level.ts";
+import { getSubscriberContext } from "../../context.ts";
+import { instrumentCallback, message } from "../../instrument.ts";
 import { OpenTelemetrySubscriber } from "../../subscribers/OpenTelemetrySubscriber.ts";
 import { InMemorySpanProcessor } from "../otel.ts";
 import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
@@ -237,6 +239,188 @@ describe("OpenTelemetrySubscriber", () => {
     expect(spans[1].events).toHaveLength(0);
     expect(spans[0].parentSpanContext?.spanId).toEqual(
       spans[1].spanContext().spanId,
+    );
+  });
+
+  it("should record fields on the span before it is entered", () => {
+    // Arrange
+    OpenTelemetrySubscriber.setGlobalDefault({ tracer });
+    const newSpan = span(Level.INFO, "test span");
+
+    // Act
+    newSpan.record("test1", "testA").record("test2", "testB").enter().exit();
+
+    // Assert
+    const spans = spanProcessor.getFinishedSpans();
+    expect(spans).toHaveLength(1);
+    expect(spans[0].name).toEqual("test span");
+    expect(spans[0].attributes).toMatchObject(
+      expect.objectContaining({
+        test1: "testA",
+        test2: "testB",
+      }),
+    );
+  });
+
+  it("should record fields on the span after it is entered", () => {
+    // Arrange
+    OpenTelemetrySubscriber.setGlobalDefault({ tracer });
+    const newSpan = span(Level.INFO, "test span");
+
+    // Act
+    newSpan.enter().record("test1", "testA").record("test2", "testB").exit();
+
+    // Assert
+    const spans = spanProcessor.getFinishedSpans();
+    expect(spans).toHaveLength(1);
+    expect(spans[0].name).toEqual("test span");
+    expect(spans[0].attributes).toMatchObject(
+      expect.objectContaining({
+        test1: "testA",
+        test2: "testB",
+      }),
+    );
+  });
+
+  it("should record fields on the span before and after it is entered", () => {
+    // Arrange
+    OpenTelemetrySubscriber.setGlobalDefault({ tracer });
+    const newSpan = span(Level.INFO, "test span");
+
+    // Act
+    newSpan.record("beforeEnter", "A").enter().record("afterEnter", "B").exit();
+
+    // Assert
+    const spans = spanProcessor.getFinishedSpans();
+    expect(spans).toHaveLength(1);
+    expect(spans[0].name).toEqual("test span");
+    expect(spans[0].attributes).toMatchObject(
+      expect.objectContaining({
+        beforeEnter: "A",
+        afterEnter: "B",
+      }),
+    );
+  });
+
+  it("should handle concurrent contexts correctly", async () => {
+    // Arrange
+    OpenTelemetrySubscriber.setGlobalDefault({ tracer });
+    async function first() {
+      const inner = span(Level.INFO, "first inner").enter();
+      await new Promise((resolve) => setTimeout(resolve, 10));
+      event(Level.INFO, "first");
+      await new Promise((resolve) => setTimeout(resolve, 20));
+      inner.exit();
+    }
+    async function second() {
+      const inner = span(Level.INFO, "second inner").enter();
+      await new Promise((resolve) => setTimeout(resolve, 20));
+      event(Level.INFO, "second");
+      await new Promise((resolve) => setTimeout(resolve, 20));
+      inner.exit();
+    }
+
+    // Act
+    const outerGuard = span(Level.INFO, "outer").enter();
+    await Promise.all([
+      instrumentCallback([message("first function")], first)(),
+      instrumentCallback([message("second function")], second)(),
+    ]);
+    outerGuard.exit();
+
+    // Assert
+    const spans = spanProcessor.getFinishedSpans();
+    expect(spans).toHaveLength(5);
+
+    expect(spans[0].name).toEqual("first inner");
+    expect(spans[0].events).toHaveLength(1);
+    expect(spans[0].events[0].name).toEqual("first");
+    expect(spans[1].name).toEqual("first function");
+    expect(spans[1].events).toHaveLength(0);
+
+    expect(spans[2].name).toEqual("second inner");
+    expect(spans[2].events).toHaveLength(1);
+    expect(spans[2].events[0].name).toEqual("second");
+    expect(spans[3].name).toEqual("second function");
+    expect(spans[3].events).toHaveLength(0);
+
+    expect(spans[4].name).toEqual("outer");
+    expect(spans[4].events).toHaveLength(0);
+
+    expect(spans[0].parentSpanContext?.spanId).toEqual(
+      spans[1].spanContext().spanId,
+    );
+    expect(spans[1].parentSpanContext?.spanId).toEqual(
+      spans[4].spanContext().spanId,
+    );
+
+    expect(spans[2].parentSpanContext?.spanId).toEqual(
+      spans[3].spanContext().spanId,
+    );
+    expect(spans[3].parentSpanContext?.spanId).toEqual(
+      spans[4].spanContext().spanId,
+    );
+  });
+
+  it.skip("should handle delayed execution on inner spans", async () => {
+    // Arrange
+    OpenTelemetrySubscriber.setGlobalDefault({ tracer });
+
+    const { promise: testCompletion, resolve: completeTest } = Promise
+      .withResolvers<true>();
+
+    const delayedFn = instrumentCallback(async function delayedFn() {
+      const inner = span(Level.INFO, "inner").enter();
+      await new Promise((resolve) => setTimeout(resolve, 10));
+      event(Level.INFO, "delayed event");
+      await new Promise((resolve) => setTimeout(resolve, 20));
+      inner.exit();
+
+      // Complete the test in the next event loop cycle
+      setTimeout(() => completeTest(true));
+    });
+
+    const main = instrumentCallback(function main() {
+      // TODO: This is a bit of a hack and only preserves the OTEL span
+      //       heirarchy... We need to provide some API with which to bind a
+      //       callback to the current tracing span, even if the current span
+      //       exits before the callback executes.
+      getSubscriberContext()!.subscriber.runInContext(
+        () => {
+          setTimeout(() => delayedFn(), 10);
+        },
+        null,
+        [],
+      );
+    });
+
+    // Act
+    main();
+    await testCompletion;
+
+    // Assert
+    const spans = spanProcessor.getFinishedSpans();
+    const processedSpans = spans.map((span) => ({
+      name: span.name,
+      id: span.spanContext().spanId,
+      parentId: span.parentSpanContext?.spanId,
+    }));
+    console.error(JSON.stringify(processedSpans, null, 2));
+    expect(spans).toHaveLength(3);
+
+    expect(spans[0].name).toEqual("main");
+    expect(spans[0].events).toHaveLength(0);
+    expect(spans[1].name).toEqual("inner");
+    expect(spans[1].events).toHaveLength(1);
+    expect(spans[1].events[0].name).toEqual("delayed event");
+    expect(spans[2].name).toEqual("delayedFn");
+    expect(spans[2].events).toHaveLength(0);
+
+    expect(spans[1].parentSpanContext?.spanId).toEqual(
+      spans[2].spanContext().spanId,
+    );
+    expect(spans[2].parentSpanContext?.spanId).toEqual(
+      spans[0].spanContext().spanId,
     );
   });
 });
